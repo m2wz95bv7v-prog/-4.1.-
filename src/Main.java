87a9efef55b5f4f469d2f6744dfb2459978ab2de
illiya <ilakokosha@icloud.com>
2026-07-06 16:26:12 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+//        String Product = "No 1: smartphone";
+//        double total = 12153.41;
+//        double sales = (total / 5 );
+//
+//        System.out.println(Product);
+//        System.out.println("total sales for 5 days is EUR " + total );
+//        System.out.println("by day is EUR " + sales );
+
+
+        String Product = "No 2: laptop";
+        double total = 10486.85;
+        double sales = (total / 7 );
+
+
+        System.out.println(Product);
+        System.out.println("total sales for 7 days is EUR " + total );
+        System.out.printf("Sales by day is EUR %.2f%n", sales);
+    }
+
+}
